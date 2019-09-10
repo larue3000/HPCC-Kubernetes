00e5e6448ac30e85e72a78cb413e23b6559146bf
xwang2713 <xiaoming.wang@lexisnexis.com>
2019-09-10 11:50:54 -0400

    update istio demo

--- istio/demo/security/rbac/delete-eclwatch.sh
+++ istio/demo/security/rbac/delete-eclwatch.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 WORK_DIR=$(dirname $0)
-. ${WORK_DIR}/../../../bin/common
+. ${WORK_DIR}/../../../../bin/common
 
 ${KUBECTL} delete -f eclwatch.yaml
 ${KUBECTL} delete -f eclwatch-bind.yaml
